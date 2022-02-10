9203f54d74bea8b127e3a24252b0b7241f61e630
Eric Wu <ericwugo@gmail.com>
2022-02-10 16:19:56 +0800

    function 有參數與預設值的運用

--- func.py
+++ func.py
@@ -1,6 +1,20 @@
+# 沒有 參數的 function
 def wash():
 	print("加水")
 	print("家洗衣精")
 	print("選轉")
 
+#有參數的 function
+def wash1(dry=False, water=8):
+	if dry :
+		print("烘衣")
+	else :
+		print("加水", water,"分滿")
+		print("家洗衣精")
+		print("選轉")
+	
+
 wash()
+wash1()
+wash1(True)
+
