ba32830b011d8027bd8a52c729d45b23d59c7a1c
Eric Wu <ericwugo@gmail.com>
2022-02-10 16:52:56 +0800

    function with return 的設計與運用

--- func.py
+++ func.py
@@ -4,7 +4,7 @@ def wash():
 	print("家洗衣精")
 	print("選轉")
 
-#有參數的 function
+#有參數 與 參數預設值的 function
 def wash1(dry=False, water=8):
 	if dry :
 		print("烘衣")
@@ -17,4 +17,23 @@ def wash1(dry=False, water=8):
 wash()
 wash1()
 wash1(True)
+print("------------")
+def add(x, y) :
+	return x + y
+
+result = add(3, 4)
+print(result)
+print("------------")
+
+def average(numbers):
+	# avg = sum(numbers) / len(numbers)
+	# return avg
+	return sum(numbers) / len(numbers)
+a = average([1, 2, 3])
+print("average list 的平均值是 :", a)
+print("第一個 List avg :",average([20,30,40]), "\n", 
+	"第二 List avg :", average([200,250,300]))
+
+
+
 
