da561a404d5b1bc00107d9de10a05d5e13261723
Eric Wu <ericwugo@gmail.com>
2022-02-16 14:06:12 +0800

    用 twilio 傳簡訊 沒有 sid

--- message.py
+++ message.py
@@ -1,17 +1,14 @@
+#用 twilio 傳 簡訊
 from twilio.rest import Client
-
-
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = 'ACa82a0768cd2bacd5f0c39d4f4c64429d'
-auth_token = 'feeb2ea3d3b96cfd80c8da0d7a429bf2'
+account_sid = 'A'
+auth_token = 'f'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                      body="EricWu Go Go Go !.",
                      from_='+18596952721',
-                     to='+886930310419'
+                     to='+8'
                  )
 
 print(message.sid)
