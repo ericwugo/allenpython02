73c5b7f845d956b57955206774adf2c038c7cd44
Eric Wu <ericwugo@gmail.com>
2022-02-16 16:25:57 +0800

    用 Pillow 套件傳成灰色圖片 將來源與結果 放不同資料夾

--- convertimg.py
+++ convertimg.py
@@ -1,10 +1,10 @@
 from PIL import Image
 import os
 
-for file in os.listdir(".") :
+for file in os.listdir("orig") : # 打開 相同路徑下的 orig 資料夾
 	if file.endswith(".jpg"):
-		image_file = Image.open(file) # image_file 是物件
+		image_file = Image.open("orig/" + file) # image_file 是物件 開啟檔案時 要加路徑 "orig/""
 		image_file = image_file.convert("L")
-		image_file.save(file[:-4] + "_gray.png")
+		image_file.save("result/" + file[:-4] + "_gray.png") # 儲存檔案 也要加路徑 "result/"
 		print(file)
 
